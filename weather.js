f650cd612b5f0695f2180d80883f3967aed2d1de
jjddm0521 <1207631630@qq.com>
2026-08-04 10:08:02 +0800

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,35 +1,18 @@
-console.log("Weather Push Started");
-2
- 
-3
+console.log("Start");
+
 const APPID = process.env.WX_APPID;
-4
 const SECRET = process.env.WX_SECRET;
-5
- 
-6
-async function getAccessToken() {
-7
- 
-8
-const url =
-9
-`https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${APPID}&secret=${SECRET}`;
-10
- 
-11
-const res = await fetch(url);
-12
- 
-13
-const data = await res.json();
-14
- 
-15
-console.log(data);
-16
+
+async function test() {
+
+  const url =
+    `https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${APPID}&secret=${SECRET}`;
+
+  const response = await fetch(url);
+
+  const data = await response.json();
+
+  console.log(data);
 }
-17
- 
-18
-getAccessToken();
+
+test();
